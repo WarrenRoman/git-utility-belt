ac0c3608aeedfcfd4c309881178b13a28d289abd
Warren Roman <warren@listio.co.za>
2017-02-08 18:28:38 +0200

    Adding git-utility-belt/src/gitutils-subdir-commit.sh

--- src/gitutils-subdir-commit.sh
+++ src/gitutils-subdir-commit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#  ---------------------------------------------------------------------------
+#  ---------------------------------------------------------------------------
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -8,11 +11,14 @@
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
-
+#
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# git-utility-belt/src/gitutils-subdir-commit.sh
+#
+#  ---------------------------------------------------------------------------
+#  ---------------------------------------------------------------------------
+#
+# Script: git-utility-belt/src/gitutils-subdir-commit.sh
 # Purpose:
 # Scans a given subdirectory and gets all git repos under the subdirectory
 # For each repo
